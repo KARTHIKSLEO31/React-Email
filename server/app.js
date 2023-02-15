18ab479b423f702cf0d63c744c4246c044839a66
karthikshanmugan <karthiksleo@gmail.com>
2023-02-15 23:10:05 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
