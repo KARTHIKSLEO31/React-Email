e75c3657cddd36c59207b555d4f57898707b0487
karthikshanmugan <karthiksleo@gmail.com>
2023-02-16 00:16:24 +0530

    nodemailer implemented

--- server/app.js
+++ server/app.js
@@ -6,3 +6,43 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/sendEmail', (req, res) => {
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'YOUR_EMAIL_ADDRESS',
+      pass: 'YOUR_EMAIL_PASSWORD',
+    },
+  });
+
+  const mailOptions = {
+    from: 'YOUR_EMAIL_ADDRESS',
+    to: 'RECIPIENT_EMAIL_ADDRESS',
+    subject: 'New Message from Contact Form',
+    html: `
+      <p>Name: ${name}</p>
+      <p>Email: ${email}</p>
+      <p>Message: ${message}</p>
+    `,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Email sent successfully');
+    }
+  });
+});
+
+app.listen(5000, () => {
+  console.log('Server started on port 5000');
+});
