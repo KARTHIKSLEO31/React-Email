79f28b693211a00d8c4ff04a4e48eab510bb50b0
karthikshanmugan <karthiksleo@gmail.com>
2023-02-16 14:13:14 +0530

    styled the form and i have added cors and setHeader is also added

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import "./ContactForm.css";
 const ContactForm = () => {
   const [email, setEmail] = useState('');
   const [subject, setSubject] = useState('');
@@ -21,19 +22,21 @@ const ContactForm = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>
+      <label class="form-label">
         Email:
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+        <input type="email" class="form-input" value={email} onChange={e => setEmail(e.target.value)} />
       </label>
-      <label>
+      <label class="form-label">
         Subject:
-        <input type="text" value={subject} onChange={e => setSubject(e.target.value)} />
+        <input type="text" class="form-input" value={subject} onChange={e => setSubject(e.target.value)} />
       </label>
-      <label>
-        Message:
-        <textarea value={message} onChange={e => setMessage(e.target.value)} />
-      </label>
-      <button type="submit">Submit</button>
+      <div class="form-message">
+        <label class="message-label">
+          Message:
+        </label>
+        <textarea class="message-input" value={message} onChange={e => setMessage(e.target.value)} />
+      </div>      
+      <button class="form-button" type="submit">Submit</button>
     </form>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const cors = require('cors');
 
 const app = express();
-dotenv.config();
+require("dotenv").config();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({extended: true}));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req,res,next) => {
+  res.setHeader("Access-Control-Allow-Origin","*");
+  next();
+});
+
 
 app.post('/send_email', (req, res) => {
   const { email,subject, message } = req.body;
